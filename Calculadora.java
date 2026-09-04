d672a8bff9f2211179eada8f57bd937a2be1ba51
Matheus <seuemail@gmail.com>
2026-09-04 20:27:19 -0300

    Primeiro commit: Adicionado o algoritimo

---
+++ Calculadora.java
@@ -0,0 +1,10 @@
+public class Calculadora {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        System.out.println("Soma: " + (a + b));
+        System.out.println("Subtracao: " + (a - b));
+        System.out.println("Multiplicacao: " + (a * b));
+        System.out.println("Divisao: " + (a / b));
+    }
+}
